1abae59c7620de1728d62cb88c34c0552800afc4
Teilnehmer <Teilnehmer@BBQTEIL-9TUAC01.Bildung.zz>
2016-12-16 10:11:54 +0100

    CardBox Constructor+fillMethode überladen, damit eine bestimmte Anzahl von Fragen geladen werden kann

--- src/ocase7/CardBox.java
+++ src/ocase7/CardBox.java
@@ -33,30 +33,32 @@ public class CardBox {
 //        this.NumberOfCards = cards.size();
 //
 //    }
-
-    
     //###############sani was here###############################
-    
     // neue CardBox wird direkt mit Karten erstellt
     // setzt Gesamtanzahl der Karten
-    
-    
     public CardBox(ArrayList<Category> categories) {
         this.cards = fillCardBox(categories);
-        this.NumberOfCards=cards.size();
-        for(Card card: cards){
+        this.NumberOfCards = cards.size();
+        for (Card card : cards) {
             System.out.println(card);
         }
     }
-       
-    
+
+    public CardBox(ArrayList<Category> categories, int numberOfQuestions) {
+        this.cards = fillCardBox(categories, numberOfQuestions);
+        this.NumberOfCards = cards.size();
+        for (Card card : cards) {
+            System.out.println(card);
+        }
+    }
+
     // füllt CardBox:
     // durchläuft KategorienArray und zieht sich dabei die dazugehörigen Fragen,    
     // überprüft ob die Frage im questionsArray schon erhalten ist,
     // wenn nicht, wird es dem questionsArray hinzugefügt
     //befüllt Card über jede QuestionID die sich nun im questionsArray befindet
     //gibt CardsArray zurück
-
+    
     public static ArrayList<Card> fillCardBox(ArrayList<Category> categories) {
         ArrayList<Question> questions = new ArrayList<>();
         ArrayList<Card> cardsi = new ArrayList<>();
@@ -70,32 +72,47 @@ public class CardBox {
         for (Question question : questions) {
             cardsi.add(new Card(question.getId()));
         }
-        
-        
+
+        return cardsi;
+
+    }
+
+    public static ArrayList<Card> fillCardBox(ArrayList<Category> categories, int numberOfQuestions) {
+        ArrayList<Question> questions = new ArrayList<>();
+        ArrayList<Card> cardsi = new ArrayList<>();
+        for (Category category : categories) {
+            for (Question question : Question.getAllQuestionsByCategoryId(category.getId())) {
+                if (!questions.contains(question)&& questions.size() <= numberOfQuestions) {
+                    questions.add(question);
+                }
+            }
+        }
+        for (Question question : questions) {
+            cardsi.add(new Card(question.getId()));
+        }
 
         return cardsi;
 
     }
 //######################sani was here, paul 2#####################################################
-    
+
     public Card nextCard(int cardBoxIndex) {
-        if(cardBoxIndex < cards.size()-1) {
-            return cards.get(cardBoxIndex +1);
+        if (cardBoxIndex < cards.size() - 1) {
+            return cards.get(cardBoxIndex + 1);
         } else {
             return cards.get(0);
         }
     }
-    
+
     //unwartbar
     public Card prevCard(int cardBoxIndex) {
-        if(cardBoxIndex > 0 ) {
-            return cards.get(cardBoxIndex -1);
+        if (cardBoxIndex > 0) {
+            return cards.get(cardBoxIndex - 1);
         } else {
-            return cards.get(cards.size()-1);
+            return cards.get(cards.size() - 1);
         }
     }
-    
-    
+
     @Override
     public String toString() {
         return "CardBox{" + "cards=" + cards + '}';
--- src/ocase7/view3/View3.java
+++ src/ocase7/view3/View3.java
@@ -27,7 +27,7 @@ import ocase7.Category;
  * @author PaulsBook
  */
 public class View3 {
-    
+
     ocase7.Card myCard;
     CardBox cardBox;
     ArrayList<Category> categories = new ArrayList<>();
@@ -41,21 +41,20 @@ public class View3 {
     Label questionNumberLabel;
     String questionNumber;
     ScrollPane answerAndQuestionScrollPane;
-    
+
     private void fillCategories() {
         categories.add(Category.getCategoryById(1));  //<-------------------------------------- GIB EINE KATEGORIE EIN
         cardBox = new CardBox(categories);
         //System.out.println(cardBox.getCards() + "########" + cardBox.getNumberOfCards());
 
     }
-    
+
     public Scene createView3() {
         fillCategories();
         Group view3Root = new Group();
         Scene view3Scene = new Scene(view3Root, Color.DEEPSKYBLUE);
         view3Scene.getStylesheets().add(getClass().getResource("/style/style.css").toExternalForm());
-        
-        
+
         myCard = cardBox.getCards().get(0);
 
         //Erstelle Boxen für Layout        
@@ -78,10 +77,10 @@ public class View3 {
 
         // setze Mindestbreite und den Border für die Fragenbox
         setMinWidthAndStyleOnQuestionBox();
-        
+
         questionTextArea = new TextArea(myCard.getQuestion().getText());
         questionTextArea.setMinHeight(500);
-        
+
         questionTextArea.focusedProperty().addListener(new ChangeListener<Boolean>() {
             @Override
             public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
@@ -100,16 +99,16 @@ public class View3 {
 
         //übergebe den gesamten Inhalt an Group
         view3Root.getChildren().add(view3ContentBox);
-        
+
         return view3Scene;
     }
-    
+
     private void setMinWidthAndStyleOnQuestionBox() {
         questionBox = new VBox();
         questionBox.setMinWidth(600);
         questionBox.setStyle("-fx-border-style: solid;" + "-fx-border-width: 1;");
     }
-    
+
     private HBox createHboxForTop() {
         HBox statusBar = new HBox();
         statusBar.setSpacing(10);
@@ -122,19 +121,19 @@ public class View3 {
         //questionNumber = "1";
         questionNumberLabel = new Label("1");
         questionNumberLabel.setFont(Font.font("Arial", 18));
-        
+
         Label seperateSign = new Label(" / ");
         seperateSign.setFont(Font.font("Arial", 18));
-        
+
         String totalNumOfQuestions = "" + cardBox.getCards().size();
         Label totalNumberOfQuestions = new Label(totalNumOfQuestions);
         totalNumberOfQuestions.setFont(Font.font("Arial", 18));
-        
+
         Button nextQuestionBtn = new Button("Vor");
         nextQuestionBtn.setMinWidth(60);
-        
+
         nextQuestionBtn.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 // lösche Elemente aus Boxen -> clear() löscht alle Elemente
@@ -152,7 +151,7 @@ public class View3 {
                 scrollPaneContent.getChildren().add(createAnswerBox());
             }
         });
-        
+
         Button prevQuestionBtn = new Button("Zurück");
         prevQuestionBtn.setMinWidth(60);
         prevQuestionBtn.setOnAction(new EventHandler<ActionEvent>() {
@@ -174,15 +173,15 @@ public class View3 {
                 scrollPaneContent.getChildren().add(createAnswerBox());
             }
         });
-        
+
         statusBar.getChildren().addAll(prevQuestionBtn, questionNumberLabel, seperateSign, totalNumberOfQuestions, nextQuestionBtn);
         return statusBar;
     }
-    
+
     private HBox createHBoxForDown() {
-        
+
         HBox buttonBar = new HBox();
-        
+
         buttonBar.setSpacing(100);
         buttonBar.setMinWidth(600);
         buttonBar.setMinHeight(40);
@@ -190,20 +189,24 @@ public class View3 {
         buttonBar.getStyleClass().add("buttonBar");
         Button followUp = new Button("Wiedervorlage");
         followUp.setMinWidth(100);
-        if (myCard.isFollowUp() == false) {
-            followUp.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
+
+        followUp.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (myCard.isFollowUp() == false) {
                     myCard.setFollowUp(true);
+                    System.out.println(myCard.isFollowUp());
+                } else {
+                    myCard.setFollowUp(false);
+                    System.out.println(myCard.isFollowUp());
                 }
-            });
-        } else {
-            followUp.setDisable(true);
-        }
+            }
+        });
+
         Button cheater = new Button("Cheater-Knopf");
         cheater.setMinWidth(100);
         if (myCard.isCheated() == false) {
-            
+
             cheater.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent event) {
@@ -212,23 +215,23 @@ public class View3 {
                     VBox anserbow = isRightAnswersBox();
                     anserbow.setDisable(true);
                     scrollPaneContent.getChildren().add(anserbow);
-                    
+
                 }
             });
-        } else{
+        } else {
             cheater.getStyleClass().add("cheaterbtn");
         }
-        
+
         Button save = new Button("Session fertig");
         save.setMinWidth(100);
-        
+
         buttonBar.getChildren().addAll(followUp, cheater, save);
-        
+
         return buttonBar;
     }
-    
+
     private VBox createAnswerBox() {
-        
+
         answersBox = new VBox();
         checkboxWithAnswerBox = new HBox();
         checkboxWithAnswerBox.setAlignment(Pos.CENTER);
@@ -245,30 +248,30 @@ public class View3 {
                         if (newValue == true) {
                             myCard.getUserAnswers().get(m).setGiven(true);
                             System.out.println(myCard.getUserAnswers().get(m).isGiven());
-                            
+
                         }
                         if (oldValue == true && newValue == false) {
                             myCard.getUserAnswers().get(m).setGiven(false);
                             System.out.println(myCard.getUserAnswers().get(m).isGiven());
                         }
-                        
+
                     }
                 });
-                
+
                 answerLabel = new Label(myCard.getUserAnswers().get(i).getText());
                 checkboxWithAnswerBox = new HBox(cb, answerLabel);
                 answersBox.getChildren().add(checkboxWithAnswerBox);
                 answersBox.setSpacing(20);
-                
+
             }
         } else {
             answersBox = isRightAnswersBox();
             answersBox.setDisable(true);
         }
-        
+
         return answersBox;
     }
-    
+
     private VBox isRightAnswersBox() {
         answersBox = new VBox();
         checkboxWithAnswerBox = new HBox();
@@ -288,7 +291,7 @@ public class View3 {
             checkboxWithAnswerBox = new HBox(cb, answerLabel);
             answersBox.getChildren().add(checkboxWithAnswerBox);
             answersBox.setSpacing(20);
-            
+
         }
         return answersBox;
     }
